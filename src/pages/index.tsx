c3ab13594717b9b9b6654a94a5a47e658b6bfd53
William Berbet <wiiliamb@hotmail.com>
2022-09-02 14:56:13 -0300

    estilizando primeira pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
-
+import Head from "next/head"
+import styles from '../../styles/home.module.scss'
+import LogoImg from '../../public/logo.svg'
+import Image from "next/image"
+import { Input } from '../components/ui/Input'
 
 export default function Home() {
   return (
-    <div>
-      <h1>App Comedoria :D </h1>
+    <>
+  <Head>
+    <title>Comedoria - Faça Seu Login!</title>
+  </Head>
+    
+    <div className={styles.containerCenter}>
+      <Image src={LogoImg} alt=""/>
+
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Digite sua senha"
+            type="password"
+          />
+        </form>
+      </div>
     </div>
+    </>
   )
 }
