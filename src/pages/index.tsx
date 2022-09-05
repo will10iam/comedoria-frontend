ac1d252009a9fc7bcf3dd24c9f01b42c6c8c4fda
William Berbet <wiiliamb@hotmail.com>
2022-09-05 14:52:56 -0300

    manutenção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Head from "next/head"
 import styles from '../../styles/home.module.scss'
 import LogoImg from '../../public/logo.svg'
 import Image from "next/image"
-import { Input } from '../components/ui/Input'
-import { Button } from '../components/ui/Button'
 import Link from "next/link"
 import { AuthContext } from '../contexts/AuthContext'
 
@@ -28,21 +26,10 @@ export default function Home() {
   </Head>
     
     <div className={styles.containerCenter}>
-      <Image className={styles.logo} src={LogoImg} alt="" width={800} height={500}/>
+      <Image className={styles.logo} src={LogoImg} alt="Logo Comedoria" width={800} height={500}/>
 
       <div className={styles.login}>
         <form onSubmit={handleLogin}>
-          <Input
-            placeholder="Digite seu email"
-            type="text"
-          />
-
-          <Input
-            placeholder="Digite sua senha"
-            type="password"
-          />
-
-          <Button type="submit" loading={false}>Acessar</Button>
 
         </form>
 
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head"
 import styles from '../../../styles/home.module.scss'
 import LogoImg from '../../../public/logo.svg'
 import Image from "next/image"
-import { Input } from '../../components/ui/Input'
-import { Button } from '../../components/ui/Button'
 import Link from "next/link"
 
 export default function SignUp() {
@@ -21,21 +19,6 @@ export default function SignUp() {
 
 
         <form>
-            <Input
-                placeholder="Digite seu nome"
-                type="text"
-            />  
-            <Input
-                placeholder="Digite seu email"
-                type="text"
-            />
-
-            <Input
-                placeholder="Digite sua senha"
-                type="password"
-            />
-
-            <Button type="submit" loading={false}>Acessar</Button>
 
         </form>
 
